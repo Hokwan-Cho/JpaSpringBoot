cca98aef1c7bb44b6386f10a36795f245f46e9fc
hokwan.cho <dev_hk@naver.com>
2022-01-20 05:33:02 +0900

    oauth 구글 추가

---
+++ src/main/java/com/kh/JpaSpringBoot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,66 @@
+package com.kh.JpaSpringBoot.config.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.JpaSpringBoot.config.auth.dto.OAuthAttributes;
+import com.kh.JpaSpringBoot.config.auth.dto.SessionUser;
+import com.kh.JpaSpringBoot.domain.users.User;
+import com.kh.JpaSpringBoot.domain.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @SneakyThrows
+    @Override
+    public OAuth2User loadUser(@NotNull OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
+
+        System.out.println("getClientRegistration : " + userRequest.getClientRegistration());
+        System.out.println("getAccessToken : " +userRequest.getAccessToken().getTokenValue());
+
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User>  delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+
+        System.out.println("@@@@@@@@@@@@@");
+        System.out.println(registrationId);
+        System.out.println(userNameAttributeName);
+
+
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),attributes.getAttributes(), attributes.getNameAttributeKey());
+
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes){
+        User user = userRepository.findByEmail(attributes.getEmail()).map(entity -> entity.update(attributes.getName(), attributes.getPicture())).orElse(attributes.toEntity());
+        System.out.println("$$$$$$$$$$$$$$$$");
+        return userRepository.save(user);
+    }
+}
--- src/main/java/com/kh/JpaSpringBoot/web/IndexController.java
+++ src/main/java/com/kh/JpaSpringBoot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.kh.JpaSpringBoot.web;
 
 
+import com.kh.JpaSpringBoot.config.auth.dto.SessionUser;
 import com.kh.JpaSpringBoot.service.posts.PostsService;
 import com.kh.JpaSpringBoot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
+
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if( user != null){
+            model.addAttribute("userName", user.getName());
+        }else{
+            System.out.println("user 없음");
+        }
         return "index";
     }
 
